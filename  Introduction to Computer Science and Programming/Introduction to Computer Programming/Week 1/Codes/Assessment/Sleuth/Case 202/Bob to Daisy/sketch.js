9173877c8047812a4cd811362635813b51e598f5
mryo_edu <edu.mryo@gmail.com>
2023-09-25 21:33:21 +0900

    vault backup: 2023-09-25 21:33:21

---
+++  Introduction to Computer Science and Programming/Introduction to Computer Programming/Week 1/Codes/Assessment/Sleuth/Case 202/Bob to Daisy/sketch.js
@@ -0,0 +1,175 @@
+/*
+
+Officer: 7360582
+CaseNum: 202-0-50981283-7360582
+
+Case 202 - The case of Bob and Daisy - stage 1
+
+That pair of notorious criminals Woz and Jobs are up to no good again.
+I’ve intercepted letters sent between them. It seems that they are
+communicating through an ingenious code in which they masquerade as
+besotted lovers, Daisy and Bob. I need you crack their code and determine
+the details of their next heist so that we can catch them in the act.
+
+Discover the hidden code by commenting out all text commands except
+those which produce magenta text. Only comment out text commands.
+Leave fill commands uncommented.
+
+There are many possible ways of investigating this case, but you
+should use ONLY the following commands:
+
+  // comments are all that are needed for this case.
+  Do NOT add any new lines of code.
+
+*/
+
+var letterFont;
+
+function preload()
+{
+	letterFont = loadFont('Ballpointprint.ttf');
+}
+
+function setup()
+{
+	createCanvas(639,676);
+	textFont(letterFont);
+	textSize(33);
+}
+
+function draw()
+{
+	background(255);
+
+	fill(0,255,0);
+	// text("your", 532,404);
+	// text("be", 21,404);
+	// text("I", 375,99);
+	fill(255,0,0);
+	// text("my", 498,174);
+	fill(0,0,255);
+	// text("make", 297,210);
+	// text("From", 18,99);
+	// text("Love", 18,509);
+	fill(0,0,0);
+	// text("alive", 313,404);
+	// text("first", 147,99);
+	// text("the", 90,99);
+	fill(0,255,0);
+	// text("the", 72,404);
+	fill(0,0,0);
+	// text("can", 385,249);
+	// text("the", 88,365);
+	// text("Daisy,", 149,33);
+	fill(255,0,255);
+	text("day", 26,210);
+	text("chosen", 25,443);
+	text("April", 124,210);
+	fill(255,0,0);
+	// text("must", 538,365);
+	// text("I", 105,133);
+	// text("I", 508,288);
+	fill(255,0,255);
+	text("second", 434,133);
+	text("date", 273,249);
+	text("in", 87,210);
+	text("is", 397,288);
+	fill(0,255,0);
+	// text("person", 228,404);
+	// text("your", 20,288);
+	// text("mu", 255,365);
+	// text("I", 345,249);
+	// text("your", 439,323);
+	fill(0,0,0);
+	// text("blessed", 145,365);
+	// text("May", 193,210);
+	// text("moment", 217,99);
+	// text("that", 385,404);
+	// text("since", 99,249);
+	fill(255,0,255);
+	text("a", 356,210);
+	fill(0,0,255);
+	// text("eye", 268,288);
+	// text("from", 219,133);
+	fill(255,0,0);
+	// text("thi", 494,249);
+	// text("ing,", 124,288);
+	fill(0,255,0);
+	// text("Ever", 24,249);
+	// text("hear", 369,323);
+	// text("kisses,", 148,509);
+	fill(0,0,0);
+	// text("nk", 524,249);
+	// text("when", 433,288);
+	fill(0,255,0);
+	// text("you", 23,174);
+	fill(0,0,255);
+	// text("love.", 318,174);
+	// text("face,", 26,133);
+	// text("luckiest", 129,404);
+	// text("are", 442,174);
+	fill(255,0,0);
+	// text("the", 134,323);
+	// text("green", 181,288);
+	// text("uiet", 208,323);
+	// text("sic", 282,365);
+	fill(0,255,0);
+	// text("lovely", 68,33);
+	// text("voice", 500,323);
+	// text("that", 537,133);
+	fill(0,0,0);
+	// text("Bob", 18,575);
+	fill(0,255,0);
+	// text("your", 470,99);
+	// text("knew", 145,133);
+	// text("q", 191,323);
+	// text("x", 82,575);
+	// text("last", 220,249);
+	fill(255,0,0);
+	// text("of", 566,249);
+	// text("I", 329,323);
+	// text("alone", 24,323);
+	// text("I", 498,365);
+	fill(0,0,255);
+	// text("s", 509,133);
+	// text("I", 445,404);
+	// text("saw", 415,99);
+	// text("our", 173,249);
+	fill(255,0,0);
+	// text("one", 200,174);
+	// text("in", 97,323);
+	// text("only", 436,249);
+	fill(0,0,255);
+	// text("few", 367,133);
+	// text("the", 373,365);
+	fill(0,0,0);
+	// text("my", 151,174);
+	// text("of", 328,365);
+	fill(0,0,255);
+	// text("am", 548,288);
+	fill(255,0,0);
+	// text("true", 256,174);
+	// text("those", 290,133);
+	// text("that", 269,323);
+	// text("that", 315,99);
+	// text("It", 349,288);
+	fill(0,0,255);
+	// text("You", 385,174);
+	fill(255,0,0);
+	// text("am", 485,404);
+	// text("I", 257,210);
+	// text("darl", 81,288);
+	// text("sunny", 547,174);
+	// text("?", 518,210);
+	// text("harp.", 430,365);
+	// text("and", 90,509);
+	// text("lovely", 531,99);
+	// text("Oh", 18,33);
+	// text("like", 27,365);
+	// text("were", 75,174);
+	// text("s.", 316,288);
+	// text("confession", 386,210);
+
+
+
+}
