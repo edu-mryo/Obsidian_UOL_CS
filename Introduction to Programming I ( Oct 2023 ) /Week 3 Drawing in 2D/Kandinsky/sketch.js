c07594c91db0069c1b109be5b870353d788d34e3
mryo_edu <edu.mryo@gmail.com>
2023-10-21 10:30:32 +0900

    vault backup: 2023-10-21 10:30:32

--- Introduction to Programming I ( Oct 2023 ) /Week 3 Drawing in 2D/Kandinsky/sketch.js
+++ Introduction to Programming I ( Oct 2023 ) /Week 3 Drawing in 2D/Kandinsky/sketch.js
@@ -8,12 +8,13 @@ function draw()
 {
 	//do your drawing here
 	strokeWeight(30);
-	//fill light red
-	fill(255, 200, 200);
+	//fill really light brown
+	fill(255, 255, 200);
 	ellipse(450, 350, 600, 600);
 
-	strokeWeight(3);
-	fill(255, 255, 255);
+	strokeWeight(5);
+	//fill really light pink
+	fill(255, 200, 200);
 	ellipse(260, 350, 100, 100);	
 
 	line( 450, 350, 450, 700); s
