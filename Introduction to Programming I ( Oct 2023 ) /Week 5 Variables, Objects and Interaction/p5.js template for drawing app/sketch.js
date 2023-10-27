a1e193052227244db8e7d89994980e0f0b5313c8
mryo_edu <edu.mryo@gmail.com>
2023-10-27 09:21:30 +0900

    vault backup: 2023-10-27 09:21:30

--- Introduction to Programming I ( Oct 2023 ) /Week 5 Variables, Objects and Interaction/p5.js template for drawing app/sketch.js
+++ Introduction to Programming I ( Oct 2023 ) /Week 5 Variables, Objects and Interaction/p5.js template for drawing app/sketch.js
@@ -1,7 +1,15 @@
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+  }
+  
+
 function setup()
 {
 	createCanvas(800, 600);
 	background(255);
+	stroke(0);
+	strokeWeight(10);
+
 
 	
 
@@ -11,8 +19,16 @@ function setup()
 
 function draw()
 {
-	stroke(0); 
-	strokeWeight(10);
 	line(mouseX,mouseY,pmouseX,pmouseY);
 
+}
+
+function mousePressed()
+{
+	stroke(getRandomInt(256),getRandomInt(256),getRandomInt(256));
+}
+
+function keyPressed()
+{
+	background(255);
 }
