d9b727d10a6e7738720f373e6b3a413410223bb7
markryo <markryo@google.com>
2023-11-06 09:13:22 +0900

    vault backup: 2023-11-06 09:13:22

--- Introduction to Programming I ( Oct 2023 ) /Week 6 Variables, Objects and Interaction (cont.)/gp2b - Using Variables/sketch.js
+++ Introduction to Programming I ( Oct 2023 ) /Week 6 Variables, Objects and Interaction (cont.)/gp2b - Using Variables/sketch.js
@@ -21,21 +21,20 @@ var mountain;
 var cloud;
 
 
-function setup()
-{
+function setup() {
 	createCanvas(1024, 576);
 	floorPos_y = 432; //NB. we are now using a variable for the floor position
 
 	//NB. We are now using the built in variables height and width
-	gameChar_x = width/2;
+	gameChar_x = width / 2;
 	gameChar_y = floorPos_y;
 
-	treePos_x = width/2;
-	treePos_y = height/2;
+	treePos_x = width / 1.9;
+	treePos_y = height / 1.72;
+
 }
 
-function draw()
-{
+function draw() {
 	background(100, 155, 255); //fill the sky blue
 
 	noStroke();
@@ -43,49 +42,50 @@ function draw()
 	rect(0, floorPos_y, height, width - floorPos_y); //draw some green ground
 
 
-	//Front facing character
-	fill("#FFE4C4")
-	ellipse(gameChar_x,gameChar_y-58,35,35);
-	
-	fill('#FFE4C4');
-	stroke('black'); // Add stroke to make the nose more obvious
-	ellipse(gameChar_x,gameChar_y-55,5,5);
 
-	noStroke();
-	fill("#0aa132");
-	rect(gameChar_x-15,gameChar_y-43,30,38);
-	rect(gameChar_x-20,gameChar_y-43,5,20);
-	rect(gameChar_x+15,gameChar_y-43,5,20);
-	
-	
-	fill('black')
-	ellipse(gameChar_x-10,gameChar_y-3,12,12);
-	ellipse(gameChar_x+10,gameChar_y-3,12,12);
-	ellipse(gameChar_x-16,gameChar_y-20,8,8);
-	ellipse(gameChar_x+16,gameChar_y-20,8,8);
-	//Front facing character end
 
 	// Tree 
 	noStroke();
 	fill(255);
 	text("tree", 800, 346);
 	// fill brown
-	fill(139,69,19);
+	fill(139, 69, 19);
 	rect(treePos_x, treePos_y, 30, 100);
 	// fill green
-	fill(0,155,0);
-	ellipse(770, 333, 60, 60);
-	ellipse(815, 333, 60, 60);
+	fill(0, 155, 0);
+	ellipse(treePos_x, 333, 60, 60);
+	ellipse(treePos_x+45, 333, 60, 60);
 	ellipse(860, 333, 60, 60);
 	ellipse(790, 290, 60, 60);
 	ellipse(835, 290, 60, 60);
 	ellipse(813, 250, 60, 60);
+	//Tree ends
 
+	//Front facing character
+	fill("#FFE4C4")
+	ellipse(gameChar_x, gameChar_y - 58, 35, 35);
+
+	fill('#FFE4C4');
+	stroke('black'); // Add stroke to make the nose more obvious
+	ellipse(gameChar_x, gameChar_y - 55, 5, 5);
+
+	noStroke();
+	fill("#0aa132");
+	rect(gameChar_x - 15, gameChar_y - 43, 30, 38);
+	rect(gameChar_x - 20, gameChar_y - 43, 5, 20);
+	rect(gameChar_x + 15, gameChar_y - 43, 5, 20);
+
+
+	fill('black')
+	ellipse(gameChar_x - 10, gameChar_y - 3, 12, 12);
+	ellipse(gameChar_x + 10, gameChar_y - 3, 12, 12);
+	ellipse(gameChar_x - 16, gameChar_y - 20, 8, 8);
+	ellipse(gameChar_x + 16, gameChar_y - 20, 8, 8);
+	//Front facing character end
 
 }
 
-function mousePressed()
-{
+function mousePressed() {
 	// Move character where user clicks on the screen.
 	gameChar_x = mouseX;
 	gameChar_y = mouseY;
