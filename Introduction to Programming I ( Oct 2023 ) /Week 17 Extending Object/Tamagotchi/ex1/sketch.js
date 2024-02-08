3b1d84c4b975f56accecc8e8b277bf6efc9a4ccd
mryo_edu <edu.mryo@gmail.com>
2024-02-09 07:28:44 +0900

    update code

---
+++ Introduction to Programming I ( Oct 2023 ) /Week 17 Extending Object/Tamagotchi/ex1/sketch.js
@@ -0,0 +1,30 @@
+
+var tamagotchi;
+
+function setup()
+{
+    
+    createCanvas(500,500);
+    noStroke();
+    
+    tamagotchi = {}
+    
+    
+}
+
+function draw()
+{
+    
+    background(0,0,0);
+    translate(width/2,height/2);
+    
+}
+
+function mouseDragged()
+{
+  
+
+}
+
+
+
